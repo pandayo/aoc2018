9b74a9588c6001acbb54ec063cb5724f050f6954
Lukas Schwabe <lukas.schwabe@gmail.com>
2018-12-16 01:03:01 +0100

    Neither done now.

--- 20181215.py
+++ 20181215.py
@@ -23,21 +23,23 @@ class Node():
         else:
             return("( - ,{0},({1},{2},{3}))".format(self.position, self.g, self.h, self.f))
 
-
-def astar(imaze, units, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
-
-    maze = []
-
-    for y in range(len(imaze)):
-        xmaze = []
-        for x in range(len(imaze[y])):
-            if imaze[y][x] == "#" or (x,y) in units:
-                xmaze.append(1)
+def printMaze(maze, start, end):
+    output = ""
+    for y in range(len(maze)):
+        for x in range(len(maze[y])):
+            if (y,x) == start or (y,x) == end:
+                if (y,x) == start:
+                    output += "s"
+                else:
+                    output += "e"
             else:
-                xmaze.append(0)
-        maze.append(xmaze)
+                output += str(maze[y][x])
+        output += "\n"
+    print(output)
 
+def astar(maze, start, end, debug = False):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+    if debug: printMaze(maze, start, end)
     # Create start and end node
     start_node = Node(None, start)
     start_node.g = start_node.h = start_node.f = 0
@@ -50,10 +52,14 @@ def astar(imaze, units, start, end):
 
     # Add the start node
     open_list.append(start_node)
-
+    counter = 1
+    
     # Loop until you find the end
     while len(open_list) > 0:
-
+        if counter % 10000 == 0:
+            printMaze(maze, start, end)
+            print(start," has not found ",end)
+            return
         # Get the current node
         current_node = open_list[0]
         current_index = 0
@@ -109,7 +115,7 @@ def astar(imaze, units, start, end):
 
             # Create the f, g, and h values
             child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0])) + ((child.position[1] - end_node.position[1]))
+            child.h = ((child.position[0] - end_node.position[0]) + (child.position[1] - end_node.position[1]))
             child.f = child.g + child.h
 
             # Child is already in the open list
@@ -118,32 +124,19 @@ def astar(imaze, units, start, end):
                     continue
 
             # Add the child to the open list
-            open_list.append(child)
+            if child not in open_list:
+                open_list.append(child)
+        counter += 1
 ###############################################################################
-
-def take_turn(unit, area, units):
-    targets = identify_targets(unit, units)
-    open_squares = identify_range(area, units, targets)
-    if (unit.x, unit.y) in open_squares:
-        return(attack(area, units, unit, open_squares[(unit.x, unit.y)]))
-    else:
-        return(move(area, units, unit, open_squares))
-
-def move(area, units, unit, open_squares):
-    range_squares = in_range(area, units, unit, open_squares)
-    reachable_squares = is_reachable(area, units, unit, range_squares)
-    nearest_squares = is_nearest(area, units, unit, reachable_squares)
-    chosen_square = chose_square(area, units, unit, nearest_squares)
-    return(mutcs(area, units, unit, chosen_square))
 
 def printMap(area, units):
     output = ""
     for y in range(len(area)):
         line_units = []
         for x in range(len(area[y])):
-            if (x,y) in units:
-                line_units.append(units[(x,y)])
-                output += units[(x,y)]["Type"]
+            if (y,x) in units:
+                line_units.append(units[(y,x)])
+                output += units[(y,x)]["Type"]
             else:
                 output += area[y][x]
         if len(line_units) > 0:
@@ -163,30 +156,70 @@ def identify_targets(unit, units):
 def identify_range(area, units, targets):
     ranges = []
     for target in targets:
-        (x,y) = targets[target]["Position"]
-        for i in range(-1,0,2):
+        (y,x) = targets[target]["Position"]
+        for i in [-1,1]:
             if not area[y+i][x] == "#":
-                if (x,y+i) not in units:
-                    ranges.append((x,y+i))
+                if (y+i,x) not in units:
+                    ranges.append((y+i,x))
             if not area[y][x+i] == "#":
-                if (x+i,y) not in units:
-                    ranges.append((x+i,y))
+                if (y,x+i) not in units:
+                    ranges.append((y,x+i))
     return(ranges)
 
 def is_reachable(area, units, unit, range_squares):
-    (s_x, s_y) = unit[1]["Position"]
+    maze = []
+    for y in range(len(area)):
+        xmaze = []
+        for x in range(len(area[y])):
+            if area[y][x] == "#" or (y,x) in units:
+                xmaze.append(1)
+            else:
+                xmaze.append(0)
+        maze.append(xmaze)
+    (s_y, s_x) = unit[1]["Position"]
     paths = {}
     for rs in range_squares:
-        paths[rs] = astar(area, units, (s_x, s_y), rs)
+        paths[rs] = astar(maze, (s_y, s_x), rs)
     return(paths)
+
+def find_attackable(pos, targets, debug = False):
+    attackable = None
+    attackableHP = 300
+    for key in sorted(targets.keys()):
+        if debug: print(key)
+        if debug: print(pos)
+        if debug: print("dis:",abs(key[0]-pos[0])+abs(key[1]-pos[1]))
+        if (abs(key[0]-pos[0]) == 0 and abs(key[1]-pos[1]) == 1) or\
+           (abs(key[0]-pos[0]) == 1 and abs(key[1]-pos[1]) == 0):
+            if attackableHP > targets[key]["HP"]:
+                attackableHP = targets[key]["HP"]
+                attackable = key
+            elif attackableHP == targets[key]["HP"]:
+                if attackable > key:
+                    attackable = key
+    return(attackable)
         
 def is_nearest(area, units, unit, reachable_squares):
     closest = {}
-    for rs in reachable_squares:
-        print(rs)
-    
+    distance = len(area)**2
+    for rpos, rpath in reachable_squares.items():
+        if rpath is None:
+            continue
+        rdis = len(rpath)-1
+        if rdis < distance:
+            closest = {}
+            distance = rdis
+            closest[rpos] = rpath
+        elif rdis == distance:
+            closest[rpos] = rpath
+    if len(closest) > 0:
+        return(closest)
+
+def chose_square(nearest_squares):
+    future_position = list(nearest_squares.keys())
+    return(nearest_squares[sorted(future_position)[0]][1])
                     
-file_name = "test.txt"
+file_name = "input.txt"
 area = [[x for x in line] for line in open(file_name, "r").read().splitlines()]
 
 elves = {}
@@ -195,29 +228,64 @@ goblins = {}
 for y in range(len(area)):
     for x in range(len(area[y])):
         if area[y][x] == "E":
-            elves[(x,y)] = {"Type":"E", "HP":200,
-                          "AP":3, "Position":(x, y)}
+            elves[(y,x)] = {"Type":"E", "HP":200,
+                          "AP":3, "Position":(y, x)}
             area[y][x] = "."
         elif area[y][x] == "G":
-            goblins[(x,y)] = {"Type":"G", "HP":200,
-                          "AP":3, "Position":(x, y)}
+            goblins[(y,x)] = {"Type":"G", "HP":200,
+                          "AP":3, "Position":(y, x)}
             area[y][x] = "."
 
 units = {**elves, **goblins}
+elves = len(elves)
+goblins = len(goblins)
+i = 1
 
 print(printMap(area, units))
-#print(sorted(units.items(), key=lambda z: z[1]["Position"]))
-##while len(elves) > 0 and len(goblins) > 0:
-##    new_units = units
-for unit in sorted(units.items(), key=lambda z: z[1]["Position"]):
-    if unit[0] in list(units.keys()):
-        t = identify_targets(unit, units)
-        r = identify_range(area, units, t)
-        reachable_squares = is_reachable(area, units, unit, r)
-        is_nearest(area, units, unit, reachable_squares)
-        
-##            area, new_units = take_turn(unit, area, units)
-##    print(printMap(area, units))
-##    units = new_units
-##    elves = [units[unit] for unit in units if units[unit]["Type"] == "E"]
-##    goblins = [units[unit] for unit in units if units[unit]["Type"] == "G"]
+while elves > 0 and goblins > 0:
+#for j in range(29):
+    print("-----{0:2d}-----".format(i))
+    new_units = units
+    for unit in sorted(units.items(), key=lambda z: z[1]["Position"]):
+        if unit[0] in new_units:
+            #print("unit :", unit)
+            t = identify_targets(unit, new_units)
+            a = find_attackable(unit[0], t)
+            if not(a is None):
+                units[a]["HP"] -= unit[1]["AP"]
+                if units[a]["HP"] < 1:
+                    if units[a]["Type"] == "E":
+                        elves -= 1
+                    else:
+                        goblins -= 1
+                    del new_units[a]
+            else:
+                r = identify_range(area, new_units, t)
+                reachable_squares = is_reachable(area, new_units, unit, r)
+                if len(reachable_squares) > 0:
+                    nearest_squares = is_nearest(area, new_units, unit, reachable_squares)
+                    if not(nearest_squares is None):
+                        cs = chose_square(nearest_squares)
+                        if not (cs is None):
+                            new_units[cs] = unit[1]
+                            new_units[cs]["Position"] = cs
+                            del new_units[unit[0]]
+                            t = identify_targets(unit, new_units)
+                            a = find_attackable(cs, t)
+                            if not(a is None):
+                                units[a]["HP"] -= unit[1]["AP"]
+                                if units[a]["HP"] < 1:
+                                    if units[a]["Type"] == "E":
+                                        elves -= 1
+                                    else:
+                                        goblins -= 1
+                                    del new_units[a]
+    i += 1
+    if i % 100 == 0:
+        print(printMap(area, units))        
+    units = new_units
+print(printMap(area, units))
+rounds = i-1
+sum_of_healths = sum([unit[1]["HP"] for unit in units.items()])
+print(sum_of_healths)
+print(sum_of_healths*rounds)
